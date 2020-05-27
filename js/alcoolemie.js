9bc937047fcd76e866c108b497465622300f3f25
Aurink GRELLET <aurink.grellet@gmail.com>
2020-05-28 00:54:23 +0200

    JS - ajout des fonctions IHM + tests

--- js/alcoolemie.js
+++ js/alcoolemie.js
@@ -74,3 +74,32 @@ function getSanction(alcoolemie) {
         return '6 points + 2 ans de prison + suspension 3 ans + stage de sensibilisation';
     }
 }
+
+
+/**
+ * Fonction qui retourne une valeur entière récupérée via
+ * window.document.querySelector(id)
+ * 
+ * @param {string} id
+ * @returns {integer}
+ */
+function getInt(id) {
+    let valeur = parseInt(getString(id));
+    if (isNaN(valeur)) {
+        window.document.querySelector(id).value = 0;
+        return 0;
+    } else {
+        return valeur;
+    }
+}
+
+
+/**
+ * Fonction qui retourne un string récupéré dans un champ via son id
+ * 
+ * @param {string} id
+ * @returns {string}
+ */
+function getString(id) {
+    return window.document.querySelector(id).value;
+}
